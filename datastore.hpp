c317e54343a571b46db0b64013ca643a02553a4d
Tom Walsh <ymmothslaw@gmail.com>
2015-10-30 00:00:13 -0400

    Add simple CRUD-based datastore

---
+++ datastore.hpp
@@ -0,0 +1,91 @@
+#ifndef DATASTORE_HPP
+#define DATASTORE_HPP
+
+#include <cstddef>
+#include <vector>
+#include <boost/optional.hpp>
+
+// Very simple CRUD-based datastore
+//
+// This file provides a simple system for storing objects in a repository
+// such that they can be referred to by lightweight references.
+//
+// It is most useful in the case where you have objects that:
+//  * Are relatively heavyweight objects -- relatively expensive to pass around
+//  * Need to be referred to from many places in the code
+//  * Often to be compared for identity, but not equality
+//  * Relatively infrequently have their properties accessed
+
+
+// A lightweight reference class to represent an object in the datastore
+template <typename T>
+class Reference
+{
+private:
+  std::size_t m_id;
+public:
+  Reference(std::size_t id) : m_id(id) {}
+  std::size_t id() const {return m_id;}
+  bool operator < (const Reference<T>& other) const {return m_id < other.m_id;}
+  bool operator == (const Reference<T>& other) const {return m_id == other.m_id;}
+  bool operator != (const Reference<T>& other) const {return m_id != other.m_id;}
+};
+
+
+// The datastore class. This is a simple in-memory implementation with no
+// persistant storage.
+template <typename T>
+class DataStore
+{
+private:
+  std::vector<boost::optional<T>> db;
+public:
+
+  // Create a new object in the datastore.
+  Reference<T> create(const T& seed);
+
+  // Read an object from the datastore, if it exists.
+  boost::optional<T> read(Reference<T> ref) const;
+
+  // Update a stored object.
+  void update(Reference<T> ref, const T& newVal);
+
+  // Remove an object from the datastore
+  void destroy(Reference<T> ref);
+
+  // Simple begin/end iterator accessors
+  typename std::vector<boost::optional<T>>::const_iterator begin() const {
+    return db.begin();
+  }
+  typename std::vector<boost::optional<T>>::const_iterator end() const {
+    return db.end();
+  }
+};
+
+template<typename T>
+Reference<T> DataStore<T>::create(const T& seed) {
+  // Always just add on to the end, never reuse an ID
+  // This is theoretically space inefficient, but practically fine for us
+  auto newId = db.size();
+  db.push_back(boost::optional<T>(seed));
+  return Reference<T>(newId);
+}
+
+template<typename T>
+boost::optional<T> DataStore<T>::read(Reference<T> ref) const {
+  return db[ref.id()];
+}
+
+template<typename T>
+void DataStore<T>::update(Reference<T> ref, const T& newVal) {
+  db[ref.id()] = newVal;
+}
+
+template<typename T>
+void DataStore<T>::destroy(Reference<T> ref) {
+  // When an object is destroyed, we just leave a "hole" in its place
+  // that will never be reused.
+  db[ref.id()] = boost::none;
+}
+
+#endif
---
+++ test/datastore.cpp
@@ -0,0 +1,47 @@
+#include "datastore.hpp"
+
+
+std::size_t getSize(DataStore<int>& ds) {
+  std::size_t count = 0;
+  for (auto maybeItem : ds) {
+    if (maybeItem) ++count;
+  }
+  return count;
+}
+
+void assertSize(DataStore<int>& ds, std::size_t expectedSize) {
+  assert (getSize(ds) == expectedSize);
+}
+
+int main()
+{
+  DataStore<int> store;
+
+  assertSize(store, 0);
+  auto a = store.create(5);
+  assertSize(store, 1);
+  auto b = store.create(10);
+  assertSize(store, 2);
+  assert(a != b);
+
+  auto c = store.read(a);
+  assert(c);
+  assert(*c == 5);
+
+  assertSize(store, 2);
+  store.update(a, 20);
+  assertSize(store, 2);
+  auto d = store.read(a);
+  assertSize(store, 2);
+  assert(d);
+  assert(*d == 20);
+
+  assert(store.read(b));
+  store.destroy(b);
+  assert(!store.read(b));
+  assertSize(store, 1);
+
+  store.create(0);
+  assertSize(store, 2);
+
+}
