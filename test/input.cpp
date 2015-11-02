e82597ff8cb852126e92ee4d7bcedd6b8d71c987
Tom Walsh <ymmothslaw@gmail.com>
2015-11-02 18:40:30 -0500

    Add unit test for input

---
+++ test/input.cpp
@@ -0,0 +1,24 @@
+#include "input.hpp"
+
+#include "teams.hpp"
+#include "games.hpp"
+
+#include <sstream>
+
+int main() {
+  TeamDB teamDB;
+  GameDB gameDB;
+
+  InputProcessor processor(teamDB, gameDB);
+  std::string input = "Arlington 3, Boston 7\n";
+  input.append("Cambridge 2, Boston 2\n");
+  input.append("Cambridge 12, Danvers 0\n");
+  input.append("Danvers 2, Arlington 2\n");
+  input.append("Arlington 1, Cambridge 4\n");
+
+  std::istringstream stream(input);
+  processor.process(stream);
+
+  assert(teamDB.allTeams().size() == 4);
+  assert(gameDB.allGames().size() == 5);
+}
