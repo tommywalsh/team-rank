991502af30aec16716f387538b4fc297b32cfb8b
Tom Walsh <ymmothslaw@gmail.com>
2022-10-16 22:12:28 -0400

    Update hockey script to Python 3

--- examples/get-college-hockey.py
+++ examples/get-college-hockey.py
@@ -1,50 +1,57 @@
-import urllib2
+from urllib.request import urlopen
 import re
 import sys
 
 def retrieve_full_page(year):
     url = "http://collegehockeyinc.com/stats/compnatfull{0}.php".format(year[-2:])
-    response = urllib2.urlopen(url)
-    return response.read()
+    response = urlopen(url)
+    return response.read().decode("utf-8")
 
 def process_page(year, page):
     state = 'prequel'
-    season = "{0}-{1}".format(int(year)-1, year)
+    season = "{0}-{1}".format(int(year)-1, year[-2:])
     for line in page.splitlines():
         if (state == 'prequel'):
             if re.search(season, line):
                 state = 'awaiting date'
-        elif re.search('Exhibition', line):
+        elif "Exhibition" in line or 'class="empty"' in line:
                 state = 'awaiting date'
         elif (state == 'awaiting date'):
-            if re.search('\d\d/\d\d/\d\d', line):
+            if line.startswith("<tr") and "stats-section" in line:
                 state = 'awaiting away team'
         elif (state == 'awaiting away team'):
-            m = re.search('"left">([^<]+)<', line)
-            awayTeam = m.group(1)
-            state = 'awaiting away score'
+            if "nowrap" in line and "class" not in line:
+                items = line.split(">")
+                items2 = items[1].split("&")
+                awayTeam = items2[0]
+                state = 'awaiting away score'
         elif (state == 'awaiting away score'):
-            # For some reason, away score and home team are on the same line!
-            m = re.search('(\d+).*?(at|vs) ([^<]+)<', line)
-            if m:
-                awayScore = m.group(1)
-                homeTeam = m.group(3)
+            if "center" in line:
+                items = line.split(">")
+                items2 = items[1].split("<")
+                if items2[0]:
+                    awayScore = items2[0]
+                    state = "awaiting home team"
+                else:
+                    # Game is not completed
+                    state = 'awaiting date'
+        elif (state == 'awaiting home team'):
+            if "nowrap" in line:
+                items = line.split(">")
+                items2 = items[1].split("&")
+                homeTeam = items2[0]
                 state = 'awaiting home score'
-            else:
-                # Game is not completed
-                state = 'awaiting date'
         elif (state == 'awaiting home score'):
-            m = re.search('\d+', line)
-            homeScore= m.group()
-            state = 'awaiting notes'
-        elif (state == 'awaiting notes'):
-            if re.search('/tr', line):
-                print homeTeam + " " + homeScore + ", " + awayTeam + " " + awayScore
-                state = 'awaiting date'
+            if "center" in line:
+                items = line.split(">")
+                items2 = items[1].split("<")
+                homeScore = items2[0]
+                print(homeTeam + " " + homeScore + ", " + awayTeam + " " + awayScore)
+                state = "awaiting away team"
 
 if len(sys.argv) != 2:
-    print "Usage: %s year".format(sys.argv[0])
-    print "where 'year' is the ending year of the season. e.g. say '2016' for the 2015-2016 season"
+    print("Usage: %s year".format(sys.argv[0]))
+    print("where 'year' is the ending year of the season. e.g. say '2016' for the 2015-2016 season")
 else:
     year = sys.argv[1]
     process_page(year, retrieve_full_page(year))
